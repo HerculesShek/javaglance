c27630e5a5c44210c574199aa0e09d968e34bf0c
Hercules Shek <hercules.shek@gmail.com>
2016-03-18 15:35:58 +0800

    bytearrayiodemo

--- iodemo/src/will/write/ByteArrayOutputDemo.java
+++ iodemo/src/will/write/ByteArrayOutputDemo.java
@@ -1,18 +1,25 @@
 package will.write;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class ByteArrayOutputDemo {
     public static void main(String[] args) throws Exception {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        String str = "I have a dream";
+        String str = "I have a dream" + System.getProperty("line.separator");
         byte[] bytes = str.getBytes();
         outputStream.write(bytes);
 
         byte[] res = outputStream.toByteArray();
         for (byte b : res) {
-            System.out.print((char)b);
+            System.out.print((char) b);
         }
+
+        OutputStream fileOut = new FileOutputStream("xz_flag.txt", true);
+        outputStream.writeTo(fileOut);
+        fileOut.close();
+        outputStream.close();
     }
 
 
