3b9d274ec24700202063c2908266470874ecfaba
Hercules Shek <hercules.shek@gmail.com>
2016-03-23 09:14:51 +0800

    thread

---
+++ multithread/src/com/will/RunnableDemo.java
@@ -0,0 +1,19 @@
+package com.will;
+
+public class RunnableDemo implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new RunnableDemo());
+        Thread t2 = new Thread(new RunnableDemo());
+        /** 也是交替打印 */
+        t1.start();
+        t2.start();
+
+    }
+}
