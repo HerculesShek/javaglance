18db5f850c39828b2cc14e4450cdc4be07e9b217
Hercules Shek <hercules.shek@gmail.com>
2016-03-24 09:46:41 +0800

    synchronized demos

--- multithread/src/com/will/mutex/amountwithmutex/nostaticfun/FetchMoney1.java
+++ multithread/src/com/will/mutex/amountwithmutex/nostaticfun/FetchMoney1.java
@@ -4,7 +4,7 @@ public class FetchMoney1 {
     public static void main(String[] args) {
         Account account = new Account(1000);
         /**
-         * 同一个账户上进行2次取款操作
+         * 同一个账户上进行2个取款操作
          */
         Thread atm = new Thread(new WithdrawThread(account, 600)); // 从atm取600
         Thread bank = new Thread(new WithdrawThread(account, 800)); // 从银行取800
---
+++ multithread/src/com/will/mutex/amountwithmutex/nostaticfun2/DepositThread.java
@@ -0,0 +1,25 @@
+package com.will.mutex.amountwithmutex.nostaticfun2;
+
+/**
+ * 取钱的动作
+ */
+public class DepositThread implements Runnable {
+    private Account account;
+    private int amount;
+
+    /**
+     * 从账户a中取钱amount金额
+     *
+     * @param account 账户
+     * @param amount  金额
+     */
+    public DepositThread(Account account, int amount) {
+        this.account = account;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("存入了" + account.deposit(amount));
+    }
+}
