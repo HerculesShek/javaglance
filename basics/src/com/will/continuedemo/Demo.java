c0dbbd4480fd6e5179fa02a519b7d8fb52139c04
Hercules Shek <hercules.shek@gmail.com>
2016-05-04 08:39:55 +0800

    switch demo

---
+++ basics/src/com/will/continuedemo/Demo.java
@@ -0,0 +1,11 @@
+package com.will.continuedemo;
+
+public class Demo {
+    public static void main(String[] args) {
+        continueTest0();
+    }
+
+    static void continueTest0() {
+
+    }
+}
