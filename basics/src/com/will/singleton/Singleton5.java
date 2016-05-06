c25445487df87f6b87795216f510193d24105cc1
Hercules Shek <hercules.shek@gmail.com>
2016-05-06 15:38:00 +0800

    update it

--- basics/src/com/will/singleton/Singleton5.java
+++ basics/src/com/will/singleton/Singleton5.java
@@ -2,13 +2,16 @@ package com.will.singleton;
 
 
 public class Singleton5 {
-    private Singleton5() {}
-
-    private static class SingletonHolder {
-        private static final Singleton5 singleton = new Singleton5();
+    private volatile static Singleton5 singleton;
+    private Singleton5() {
     }
 
     public static synchronized Singleton5 getInstance() {
-        return SingletonHolder.singleton;
+        if (singleton == null) {
+            synchronized (Singleton5.class) {
+                if (singleton == null) singleton = new Singleton5();
+            }
+        }
+        return singleton;
     }
 }
